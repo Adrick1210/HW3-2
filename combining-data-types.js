f619b28e295fdf459cf284549f5241f504a67b62
Christian Alexander <christian.alexanderato@gmail.com>
2023-11-24 06:50:45 -0800

    working on section 3

--- combining-data-types.js
+++ combining-data-types.js
@@ -60,11 +60,121 @@ crayonSelector(crayonBox.color[2]);
 
 // callback option to options function
 const options = () => {
-  console.log('select a song')
-}
+  console.log("select a song");
+};
 
 const powerButton = (option) => {
   option();
 };
 
 powerButton(options);
+
+//======================================================
+// Vending Machine
+//======================================================
+
+const vendingMachine = {
+  snacks: [
+    { name: "Hershey", price: 2 },
+    { name: "lays", price: 1.5 },
+    { name: "gummy bears", price: 0.5 },
+  ],
+  vend: (num) => {
+    if (num === 0) {
+      console.log(vendingMachine.snacks[0].name)
+    } else if (num === 1) {
+      console.log(vendingMachine.snacks[1].name)
+    } else if (num === 2) {
+      console.log(vendingMachine.snacks[2].name)
+    }
+  }
+};
+// console.log(vendingMachine.snacks[1]);
+vendingMachine.vend(0);
+vendingMachine.vend(2);
+
+//======================================================
+// Callbacks
+//======================================================
+
+// add function
+const add = (num1, num2) => {
+  console.log(num1 + num2);
+}
+
+//subtract function
+const subtract = (num1, num2) => {
+  console.log(num1 - num2);
+}
+
+// multiply function
+const multiply = (num1, num2) => {
+  console.log(num1 * num2);
+}
+
+// divide function
+const divide = (num1, num2) => {
+  console.log(num1 / num2);
+}
+
+// calculate function
+// const calculate = (num1, num2, operates) => {
+    
+// }
+// comeback
+
+// cleanup code
+
+// bar(); // moved to after the function declaration
+// const bar = () => {
+//     console.log('bar here');
+// }
+// bar(); // will run
+
+// foo() // moved to after the function declaration
+// const foo = () => {
+//     console.log('foo here');
+// }
+// foo(); // will run
+
+// errors
+
+// foo();
+
+// const foo ()=>{
+//     console.log('hi');
+// }
+
+// foo is called before the function itself is declared. If foo()
+// is called after the function, it should run.
+
+//======================================================
+// Array Methods with Callbacks
+//======================================================
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0]
+
+const panagram = ['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog']
+
+// every
+let equalZero = nums.every((num) => {
+  for (let num of nums) {
+    if(num >= 0){
+      return true
+    } else {
+      return false;
+    }
+  }
+});
+console.log(equalZero);
+
+let eightChar = panagram.every((str) => {
+  for (let str of panagram) {
+    if(str.length > 8) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+})
+console.log(eightChar);
