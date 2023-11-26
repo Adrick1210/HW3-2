6cc3b0cd5ed9526ac7ec7c7533c8218f53264c73
Christian Alexander <christian.alexanderato@gmail.com>
2023-11-25 18:42:17 -0800

    Finished methods

--- combining-data-types.js
+++ combining-data-types.js
@@ -81,13 +81,13 @@ const vendingMachine = {
   ],
   vend: (num) => {
     if (num === 0) {
-      console.log(vendingMachine.snacks[0].name)
+      console.log(vendingMachine.snacks[0].name);
     } else if (num === 1) {
-      console.log(vendingMachine.snacks[1].name)
+      console.log(vendingMachine.snacks[1].name);
     } else if (num === 2) {
-      console.log(vendingMachine.snacks[2].name)
+      console.log(vendingMachine.snacks[2].name);
     }
-  }
+  },
 };
 // console.log(vendingMachine.snacks[1]);
 vendingMachine.vend(0);
@@ -100,28 +100,29 @@ vendingMachine.vend(2);
 // add function
 const add = (num1, num2) => {
   console.log(num1 + num2);
-}
+};
 
 //subtract function
 const subtract = (num1, num2) => {
   console.log(num1 - num2);
-}
+};
 
 // multiply function
 const multiply = (num1, num2) => {
   console.log(num1 * num2);
-}
+};
 
 // divide function
 const divide = (num1, num2) => {
   console.log(num1 / num2);
-}
+};
 
 // calculate function
-// const calculate = (num1, num2, operates) => {
-    
-// }
-// comeback
+const calculate = (num1, num2, operates) => {
+  return operates(num1, num2);
+};
+calculate(8, 12, multiply);
+calculate(2, 4, divide);
 
 // cleanup code
 
@@ -152,29 +153,76 @@ const divide = (num1, num2) => {
 // Array Methods with Callbacks
 //======================================================
 
-const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0]
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0];
 
-const panagram = ['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog']
+const panagram = [
+  "The",
+  "quick",
+  "brown",
+  "fox",
+  "jumps",
+  "over",
+  "the",
+  "lazy",
+  "dog",
+];
 
 // every
-let equalZero = nums.every((num) => {
-  for (let num of nums) {
-    if(num >= 0){
-      return true
-    } else {
-      return false;
-    }
-  }
-});
+let equalZero = nums.every((num) => nums[num] >= 0);
 console.log(equalZero);
 
-let eightChar = panagram.every((str) => {
-  for (let str of panagram) {
-    if(str.length > 8) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-})
+let eightChar = panagram.every((str) => str.length < 8);
 console.log(eightChar);
+
+// filter
+let lessFour = nums.filter((num) => num <= 4);
+console.log(lessFour);
+
+let evenLength = panagram.filter((str) => str.length % 2 === 0);
+console.log(evenLength);
+
+// find
+let findFive = nums.find((num) => num % 5 === 0);
+console.log(findFive);
+
+let wordFive = panagram.find((str) => str.length >= 5);
+console.log(wordFive);
+
+// find index
+let indexThree = nums.findIndex((num) => num % 3 === 0);
+console.log(indexThree);
+
+let wordIndex = panagram.findIndex((str) => str.length >= 2);
+console.log(wordIndex);
+
+// for each
+let byThree = nums.forEach((num) => console.log(num * 3));
+
+let explanation = panagram.forEach((str) => console.log(str + "!"));
+
+// Nothing happened to the original array as the new values weren't
+// modified to the original array.
+
+// I can store those new values in a new array I declare.
+
+// map
+let newArr = [];
+let bigNum = nums.map((num) => newArr.push(num * 100));
+console.log(newArr);
+
+let upArr = [];
+let upperCase = panagram.map((str) => upArr.push(str.toUpperCase()));
+console.log(upArr);
+
+// Nothing has changed the original array still. As I've stored the new values
+// into a new array.
+
+// I can store values from a map method inside of a new array.
+
+// some
+let bySeven = (num) => num % 7 === 0;
+console.log(nums.some(bySeven));
+
+let isA = panagram.some((letter) => letter.includes("a")
+);
+console.log(isA);
